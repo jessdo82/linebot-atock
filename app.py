f9bcd21bd05eda816cc2d8cf16faab693dddb8ab
jessdo82 <a6567777@gmail.com>
2025-03-21 14:38:50 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import os
+
+app = Flask(__name__)
+
+# 設定 LINE Bot 的 Access Token 和 Secret
+line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=f"您說了：{event.message.text}")
+    )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
